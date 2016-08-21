a29e70c7d99c11563d82ea7d2a3c30c99390f12f
Daniel Bornkessel <daniel@bornkessel.com>
2016-08-21 21:55:48 +0200

    Minimal dom modification

--- src/main.rs
+++ src/main.rs
@@ -1,44 +1,48 @@
 extern crate html5ever;
+#[macro_use(qualname,ns,atom)]
+extern crate string_cache;
+#[macro_use(format_tendril)] extern crate tendril;
 
 use html5ever::{ParseOpts, parse_document};
 use html5ever::tree_builder::TreeBuilderOpts;
-use html5ever::rcdom::{RcDom, Node};
+use html5ever::rcdom::{RcDom, Handle, Node};
 use html5ever::rcdom::NodeEnum::Element;
 use html5ever::serialize::{SerializeOpts, serialize};
 use html5ever::tendril::TendrilSink;
 use std::io;
+use html5ever::tree_builder::TreeSink;
+use html5ever::tree_builder::interface::NodeOrText;
+use tendril::Tendril;
 
 fn main() {
     let opts = ParseOpts {
-        tree_builder: TreeBuilderOpts {
-            drop_doctype: true,
-            ..Default::default()
-        },
+        tree_builder: TreeBuilderOpts { drop_doctype: true, ..Default::default() },
         ..Default::default()
     };
 
     let stdin = io::stdin();
-    let dom = parse_document(RcDom::default(), opts)
-        .from_utf8()
-        .read_from(&mut stdin.lock())
-        .unwrap();
+    let mut dom = parse_document(RcDom::default(), opts)
+                      .from_utf8()
+                      .read_from(&mut stdin.lock())
+                      .unwrap();
 
-    let document = dom.document.borrow();
-    let html = document.children[0].borrow();
-    for e in html.children.iter() {
-        if let Element(ref qualname, _, _) = e.borrow().node {
-            if qualname.local.eq_str_ignore_ascii_case("body") {
-                for e in e.borrow().children.iter() {
-                    if let Element(_, _, ref mut attributes) = e.borrow_mut().node {
-                        if attributes.len() > 0 {
-                            attributes[0].value.push_tendril(&From::from("#anchor"));
-                        }
-                    }
-                }
+    let mut x: Option<Handle> = None;
+    {
+        let document = dom.document.borrow();
+        let html = document.children[0].borrow();
+        for e in html.children.iter() {
+            if let Element(ref qualname, _, _) = e.borrow().node {
+                x = Some(e.clone());
+                break;
             }
         }
     }
 
+    if let Some(xval) = x {
+        let attr = html5ever::Attribute{name: qualname!("", "name"), value: format_tendril!("xxx")};
+        let myElement = dom.create_element(qualname!(html, "a"), vec![attr]);
+        dom.append(xval, NodeOrText::AppendNode(myElement));
+    }
     let mut bytes = vec![];
     serialize(&mut bytes, &dom.document, SerializeOpts::default()).unwrap();
     let result = String::from_utf8(bytes).unwrap();
