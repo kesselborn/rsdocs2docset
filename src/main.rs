9a84e01b3aca06897076590d2eda127c8e1cfe30
Daniel Bornkessel <daniel@bornkessel.com>
2016-11-19 16:13:37 +0100

    First primitive version with recursive dirs

--- src/main.rs
+++ src/main.rs
@@ -7,8 +7,9 @@ use html5ever::serialize::{SerializeOpts, serialize};
 use html5ever::tendril::TendrilSink;
 use html5ever::tree_builder::TreeBuilderOpts;
 
-use std::fs::File;
-use std::io::Write;
+use std::fs::{self, DirBuilder, DirEntry, File, ReadDir};
+use std::io::{self, Error, ErrorKind, Write};
+use std::path::Path;
 use std::string::String;
 
 use docsrs2docset::dom::{manipulator, parser};
@@ -17,19 +18,50 @@ use docsrs2docset::dom::{manipulator, parser};
 // #### read https://thesquareplanet.com/blog/rust-tips-and-tricks/ Partial matching
 
 fn main() {
-    docset_file_from_rs_doc("index.html".to_string(), "index-out.html".to_string())
+    let out_dir = "out-dir".to_string();
+
+    if let Err(e) = docset_tree_from_rs_doc_tree(Path::new("target/doc"),
+                                                 &out_dir,
+                                                 &docset_file_from_rs_doc) {
+        println!{"error: {}", e}
+    }
 }
 
-fn docset_file_from_rs_doc(input: String, output: String) {
+fn docset_tree_from_rs_doc_tree(source_dir: &Path, out_dir: &String, cb: &Fn(&DirEntry, &String))
+                                -> io::Result<()> {
+    if source_dir.is_dir() {
+        for entry in try!(fs::read_dir(source_dir)) {
+            let entry = try!(entry);
+            let path = entry.path();
+            if path.is_dir() {
+                try!(docset_tree_from_rs_doc_tree(&path, &out_dir, cb));
+            } else {
+                cb(&entry, &out_dir);
+            }
+        }
+    }
+    Ok(())
+}
+
+fn docset_file_from_rs_doc(input: &DirEntry, output_prefix: &String) {
+    let out_dir = Path::new(output_prefix).join(input.path());
+    let output = out_dir.join(input.file_name());
+
+    DirBuilder::new()
+        .recursive(true)
+        .create(out_dir)
+        .unwrap();
+
+
     let opts = ParseOpts {
         tree_builder: TreeBuilderOpts { drop_doctype: true, ..Default::default() },
         ..Default::default()
     };
 
-    let mut in_file = match File::open(&input) {
+    let mut in_file = match File::open(&input.path()) {
         Ok(f) => f,
         Err(e) => {
-            println!("Error opening {}: {}", input, e);
+            println!("Error opening {}: {}", input.path().display(), e);
             return;
         }
     };
@@ -49,15 +81,15 @@ fn docset_file_from_rs_doc(input: String, output: String) {
     let mut out_file = match File::create(&output) {
         Ok(f) => f,
         Err(e) => {
-            println!("Error creating file {}: {}", output, e);
+            println!("Error creating file {}: {}", output.display(), e);
             return;
         }
     };
 
     match out_file.write_all(result.as_ref()) {
         Ok(_) => println!("Successfully added docset links from {} -> {}",
-                          input,
-                          output),
-        Err(e) => println!("Error writing {}: {}", output, e),
+                          input.path().display(),
+                          output.display()),
+        Err(e) => println!("Error writing {}: {}", output.display(), e),
     }
 }
