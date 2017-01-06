c66466e8139b5832cb5131c6584d1fd734ee2d88
Daniel Bornkessel <daniel@bornkessel.com>
2017-01-06 01:03:02 +0100

    Simplify main file

--- src/main.rs
+++ src/main.rs
@@ -17,16 +17,10 @@ use std::ffi::OsStr;
 use std::fs::{self, DirBuilder, DirEntry, File};
 use std::io::{self,Write};
 use std::path::Path;
-use std::string::String;
 
 use rsdocs2docset::dom::{manipulator, parser};
 
 type Result<T> = std::result::Result<T, RsDoc2DocsetError>;
-
-static DB_PATH: &'static str = "/tmp/foo.dsidx";
-
-// #### read https://thesquareplanet.com/blog/rust-tips-and-tricks/ Cloning iterators
-// #### read https://thesquareplanet.com/blog/rust-tips-and-tricks/ Partial matching
 
 quick_error! {
     #[derive(Debug)]
@@ -37,6 +31,11 @@ quick_error! {
                 description(err.description())
         }
         Utf8(err: std::string::FromUtf8Error) {
+            from()
+                cause(err)
+                description(err.description())
+        }
+        Sqlite(err: sqlite::Error) {
             from()
                 cause(err)
                 description(err.description())
@@ -64,51 +63,39 @@ fn main() {
              .takes_value(true))
         .get_matches();
 
-    create_db();
-
-    if let Err(e) = docset_from_rs_doc_tree(Path::new(args.value_of("indir").unwrap()),
-    format!("{}.docset/Contents/Resources/Documents/", &args.value_of("name").unwrap()).as_str(),
-	&annotate_file) {
-		println!{"error: {}", e}
-	}
-
-	let info_plist = format!(include_str!("Info.plist.tmpl"), name = args.value_of("name").unwrap(), identifier = String::from(args.value_of("name").unwrap()).replace(" ", "-").to_lowercase());
-
-	let plist_path_str = format!("{}.docset/Contents/Info.plist", &args.value_of("name").unwrap());
-    let plist_path = Path::new(&plist_path_str);
-	if let Err(e) = File::create(plist_path).and_then(|mut x| x.write_all(info_plist.as_ref())) {
-		println!{"error: {}", e}
-	}
-
-    let icon = include_bytes!("icon.png");
-	let icon_path_str = format!("{}.docset/icon.png", &args.value_of("name").unwrap());
-    let icon_path = Path::new(&icon_path_str);
-	if let Err(e) = File::create(icon_path).and_then(|mut x| x.write_all(icon)) {
-		println!{"error: {}", e}
-	}
-
-    let icon_2x = include_bytes!("icon@2x.png");
-	let icon_2x_path_str = format!("{}.docset/icon_2x_.png", &args.value_of("name").unwrap());
-    let icon_2x_path = Path::new(&icon_2x_path_str);
-	if let Err(e) = File::create(icon_2x_path).and_then(|mut x| x.write_all(icon_2x)) {
-		println!{"error: {}", e}
-	}
-
-	let db_path_str = format!("{}.docset/Contents/Resources/docSet.dsidx", &args.value_of("name").unwrap());
-    if let Err(e) = fs::rename(DB_PATH, db_path_str) {
-        panic!("error moving db: {}", e)
-    }
+    create_docset(args.value_of("indir").unwrap(), args.value_of("name").unwrap()).expect("error creating docset");
+    println!("\n{}.docset successfully created!", args.value_of("name").unwrap())
 }
 
-fn create_db() {
-    if let Err(e) = sqlite::open(Path::new(DB_PATH))
-        .and_then(|c| c.execute("CREATE TABLE searchIndex(id INTEGER PRIMARY KEY, name TEXT, type TEXT, path TEXT);")) {
-        panic!("error creating db: {}", e);
-    }
+fn create_docset(indir: &str, name: &str) -> Result<()> {
+    let db_filename = format!("{}.docset/Contents/Resources/docSet.dsidx", name);
+    let db_path = Path::new(&db_filename);
+
+    try!(write_file(Path::new(format!("{}.docset/Contents/Info.plist", name).as_str()), format!(include_str!("Info.plist.tmpl"), name = name, identifier = name.replace(" ", "-").to_lowercase()).as_ref()));
+    try!(write_file(Path::new(format!("{}.docset/icon.png", name).as_str()), include_bytes!("icon.png")));
+    try!(write_file(Path::new(format!("{}.docset/icon@2x.png", name).as_str()), include_bytes!("icon@2x.png")));
+
+    try!(write_file(db_path, "".as_bytes()));
+    try!(sqlite::open(db_path).and_then(|c| c.execute("CREATE TABLE searchIndex(id INTEGER PRIMARY KEY, name TEXT, type TEXT, path TEXT);")));
+
+    try!(docset_from_rs_doc_tree(&Path::new(indir), format!("{}.docset/Contents/Resources/Documents/", name).as_str(), Path::new(db_path), &annotate_file));
+
+    Ok(())
 }
 
-fn docset_from_rs_doc_tree(source_dir: &Path, out_dir: &str,
-                           annotate_file: &Fn(&DirEntry, &str) -> Result<()>)
+fn write_file(path: &Path, data: &[u8]) -> Result<()> {
+    let dir = path.parent().unwrap();
+
+    try!(DirBuilder::new()
+         .recursive(true)
+         .create(dir));
+
+    try!(File::create(path).and_then(|mut f| f.write_all(data)));
+    Ok(())
+}
+
+fn docset_from_rs_doc_tree(source_dir: &Path, out_dir: &str, db_path: &Path,
+                           annotate_file: &Fn(&DirEntry, &str, &Path) -> Result<()>)
     -> Result<()> {
         if !source_dir.exists() {
             return Err(io::Error::new(io::ErrorKind::NotFound, format!("{} does not exist", source_dir.to_str().unwrap())).into());
@@ -118,32 +105,25 @@ fn docset_from_rs_doc_tree(source_dir: &Path, out_dir: &str,
             for entry in fs::read_dir(source_dir)? {
                 let entry = entry?;
                 if entry.path().is_dir() {
-                    try!(docset_from_rs_doc_tree(&entry.path(), &out_dir, annotate_file));
+                    try!(docset_from_rs_doc_tree(&entry.path(), &out_dir, &db_path, annotate_file));
                 } else {
-                    try!(annotate_file(&entry, &out_dir));
+                    try!(annotate_file(&entry, &out_dir, &db_path));
                 }
             }
         }
         Ok(())
     }
 
-fn annotate_file(in_file: &DirEntry, output_prefix: &str) -> Result<()> {
-    let out_dir = Path::new(output_prefix).join(in_file.path().parent().unwrap());
-    let out_file = out_dir.join(in_file.file_name());
-
-    try!(DirBuilder::new()
-         .recursive(true)
-         .create(out_dir));
+fn annotate_file(in_file: &DirEntry, output_prefix: &str, db_path: &Path) -> Result<()> {
+    let out_file = Path::new(output_prefix).join(in_file.path());
 
     if in_file.path().extension() != Some(OsStr::new("html")) {
+        try!(write_file(Path::new(&out_file), "".as_bytes()));
         try!(fs::copy(in_file.path(), &out_file));
-        println!("{:70} | ", in_file.path().display());
-        //print!(".")
+        //println!("{:70} | ", in_file.path().display());
+        print!("."); io::stdout().flush().unwrap();
     } else {
-        let opts = ParseOpts {
-            tree_builder: TreeBuilderOpts { drop_doctype: true, ..Default::default() },
-            ..Default::default()
-        };
+        let opts = ParseOpts { tree_builder: TreeBuilderOpts { drop_doctype: true, ..Default::default() }, ..Default::default() };
 
         let mut dom = parse_document(RcDom::default(), opts)
             .from_utf8()
@@ -151,26 +131,18 @@ fn annotate_file(in_file: &DirEntry, output_prefix: &str) -> Result<()> {
 
         let entries = parser::find_entry_elements(&mut dom);
         for entry in entries.iter().filter_map(|x| x.as_ref()) {
-            print!("{:70} | ", in_file.path().display());
-            println!("{}", entry);
+            //println!("{:70} | {}", in_file.path().display(), entry);
 
             let sql_cmd = format!("INSERT OR IGNORE INTO searchIndex(name, type, path) VALUES (\"{}\", \"{}\", \"{}#{}\");", entry.entry_name, entry.entry_type, in_file.path().to_str().unwrap(), entry.anchor_name);
-            if let Err(e) = sqlite::open(Path::new(DB_PATH))
-                .and_then(|c| c.execute(&sql_cmd)) {
-                    panic!("error executing {}: {}", &sql_cmd, e);
-                }
-
+            try!(sqlite::open(db_path).and_then(|c| c.execute(&sql_cmd)))
         }
         manipulator::add_dash_links(&mut dom, &entries);
 
         let mut bytes = vec![];
         try!(serialize(&mut bytes, &dom.document, SerializeOpts::default()));
+        try!(write_file(&out_file, bytes.as_ref()));
 
-        let result = String::from_utf8(bytes)?;
-
-        File::create(&out_file)?.write_all(result.as_ref())?;
-
-        //print!(".")
+        print!("o"); io::stdout().flush().unwrap();
     }
 
     Ok(())
