a92e0dfb921d7ac56ad2fe605c7ddf09af9f63b1
Daniel Bornkessel <daniel@bornkessel.com>
2017-01-06 10:24:11 +0100

    Some comments; refactored if statement

--- src/dom/parser.rs
+++ src/dom/parser.rs
@@ -83,11 +83,13 @@ fn find_element_with_class(h: &Handle, class_value: &str) -> Option<Handle> {
         if let Element(_, _, ref attrs) = e.borrow().node {
             if attrs.iter()
                     .find(|ref attr| attr.name == qualname!("", "class"))
-                    .and_then(|attr| Some(attr.value.to_string() == class_value))
-                    .unwrap_or(false) {
+                    .and_then(|attr| Some(attr.value.to_string() == class_value)) !=
+               None {
                 return Some(e.clone());
             }
         }
+
+        // recurse down and check children
         if let Some(e) = find_element_with_class(e, class_value) {
             return Some(e);
         }
@@ -99,6 +101,7 @@ fn get_text(h: &Handle) -> Option<String> {
     let mut text_tokens = Vec::new();
     let node = h.borrow();
 
+    // get all text from current and all descendent elements: <span>hallo <a href="http://heise.de">heise</a>!</span> -> hallo heise !
     for e in node.children.iter() {
         match e.borrow().node {
             Text(ref t) => text_tokens.push(t.to_string()),
@@ -106,6 +109,7 @@ fn get_text(h: &Handle) -> Option<String> {
         }
     }
 
+    // adjust extracted text: mainly remove white space and some unicode characters
     if text_tokens.len() > 0 {
         Some(String::from(text_tokens.join(" ").trim())
                  .replace("\u{a0}", "")
