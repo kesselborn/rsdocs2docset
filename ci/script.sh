47f03c5687ecab63bb987bcf36141c424add0124
Daniel Bornkessel <daniel@bornkessel.com>
2017-01-09 11:17:12 +0100

    Adjust ci script (verbose + don't run the binary)

--- ci/script.sh
+++ ci/script.sh
@@ -4,8 +4,8 @@ set -ex
 
 # TODO This is the "test phase", tweak it as you see fit
 main() {
-    cross build --target $TARGET
-    cross build --target $TARGET --release
+    cross build --verbose --target $TARGET
+    cross build --verbose --target $TARGET --release
 
     if [ -n $DISABLE_TESTS ]; then
         return
@@ -13,9 +13,6 @@ main() {
 
     cross test --target $TARGET
     cross test --target $TARGET --release
-
-    cross run --target $TARGET
-    cross run --target $TARGET --release
 }
 
 # we don't run the "test phase" when doing deploys
